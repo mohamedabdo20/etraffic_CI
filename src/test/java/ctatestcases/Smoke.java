47e89776326494b52726172f70a01878fbe9da0f
Mohamed Abdo <mohamed.abdo20@gmail.com>
2019-02-26 13:57:34 +0400

    add smoke one

---
+++ src/test/java/ctatestcases/Smoke.java
@@ -0,0 +1,48 @@
+package ctatestcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class Smoke {
+	WebDriver driver;
+	@Test
+	public void openchrome()
+	{
+	
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://www.googl.com");
+		driver.quit();
+		
+	}
+	@Test
+	public void openIE()
+	{
+		System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("http://www.googl.com");
+		driver.quit();
+	}
+	@Test
+	public void openFirefox()
+	{
+		System.setProperty("webdriver.ie.driver", "./drivers/IEDriverServer.exe");
+		driver = new InternetExplorerDriver();
+		driver.get("http://www.googl.com");
+		driver.quit();
+		
+	}
+	@Test
+	public void FailedTC()
+	{
+		Assert.assertEquals("1","2");
+		
+	}
+	
+	
+}
