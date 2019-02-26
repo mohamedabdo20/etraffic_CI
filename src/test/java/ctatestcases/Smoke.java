5804ecb1b820fbf69acf235f976648af468fb3e1
Mohamed Abdo <mohamed.abdo20@gmail.com>
2019-02-26 15:10:20 +0400

    smoke2

--- src/test/java/ctatestcases/Smoke.java
+++ src/test/java/ctatestcases/Smoke.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.testng.annotations.Test;
 
 import junit.framework.Assert;
@@ -32,7 +33,9 @@ public class Smoke {
 	public void openIE()
 	{
 		System.setProperty("webdriver.ie.driver", "./drivers/IEDriverServer.exe");
-		driver = new InternetExplorerDriver();
+		InternetExplorerOptions options =new InternetExplorerOptions();
+		options.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,true); 
+		driver = new InternetExplorerDriver(options);
 		driver.get("https://tst12c:7791/traffic/faces/jsf/auth/login.jsf");
 		driver.quit();
 		
