486a3cbd46fc2c6174edcdddbf2e974198ffc484
Mohamed Abdo <mohamed.abdo20@gmail.com>
2019-03-04 10:24:23 +0400

    Prepare XML files and smoke of IE

--- src/test/java/isoft/etraffic/ctatestcases/Smoke.java
+++ src/test/java/isoft/etraffic/ctatestcases/Smoke.java
@@ -1,22 +1,35 @@
 package isoft.etraffic.ctatestcases;
 
-import isoft.etraffic.testbase.TestBase;
-import junit.framework.Assert;
+import java.util.concurrent.TimeUnit;
 
-public class Smoke extends TestBase {
-	//WebDriver driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class Smoke   {
+	WebDriver driver;
+	@Test
+	public void IETest()
+	{
+		System.setProperty("webdriver.ie.driver", "./drivers/IEDriverServer.exe");
+		driver = new InternetExplorerDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://tst12c:7791/traffic/faces/jsf/auth/login.jsf");
+		driver.get("javascript:document.getElementById('overridelink').click();");
+		driver.quit();
+	}
 	
-	public void FirstTest()
-	{}
-	
-	
+	@Test
 	public void PassedTC()
 	{
 		Assert.assertEquals("1","1");}
 	
-	
+	/*@Test
 	public void FailedTC()
 	{
-		Assert.assertEquals("1","2");}
+		Assert.assertEquals("1","2");}*/
 	
 }
